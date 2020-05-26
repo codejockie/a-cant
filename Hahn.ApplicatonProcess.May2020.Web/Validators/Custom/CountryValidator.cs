eee0a5b7c482a83bf5c86e5dc8fb503cacfc07e1
John C. Kennedy <devjckennedy@gmail.com>
2020-05-26 12:51:30 +0900

    Country validator

---
+++ Hahn.ApplicatonProcess.May2020.Web/Validators/Custom/CountryValidator.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using FluentValidation.Validators;
+
+namespace Hahn.ApplicatonProcess.May2020.Web.Validators.Custom
+{
+    public class CountryValidator : PropertyValidator
+    {
+        private readonly HttpClient _httpClient;
+
+        public CountryValidator(HttpClient httpClient) : base("{PropertyName} must be a valid country.")
+        {
+            _httpClient = httpClient;
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var countryName = (string)context.PropertyValue;
+            //var response = await _httpClient.GetAsync($"https://restcountries.eu/rest/v2/name/{countryName}?fullText=true");
+            bool isValid = false;
+
+            Task.Run(async () =>
+            {
+                var response = await _httpClient.GetAsync($"https://restcountries.eu/rest/v2/name/{countryName}?fullText=true");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    isValid = false;
+                }
+            });
+
+            return isValid;
+        }
+    }
+}
---
+++ Hahn.ApplicatonProcess.May2020.Web/Validators/Custom/CustomValidatorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FluentValidation;
+
+namespace Hahn.ApplicatonProcess.May2020.Web.Validators.Custom
+{
+    public static class CustomValidatorExtensions
+    {
+        public static IRuleBuilderOptions<T, string> ValidCountry<T>(
+        this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new CountryValidator(new System.Net.Http.HttpClient()));
+        }
+    }
+}
