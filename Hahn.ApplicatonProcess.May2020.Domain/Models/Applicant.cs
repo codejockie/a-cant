72bca5062f2b4d65d869f913a21556fd753f77d5
John C. Kennedy <devjckennedy@gmail.com>
2020-05-26 12:49:58 +0900

    Applicant model

---
+++ Hahn.ApplicatonProcess.May2020.Domain/Models/Applicant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Hahn.ApplicatonProcess.May2020.Domain.Models.Base;
+
+namespace Hahn.ApplicatonProcess.May2020.Domain.Models
+{
+    public class Applicant : Entity
+    {
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public string Address { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public string EmailAddress { get; set; }
+        public int Age { get; set; }
+        public bool Hired { get; set; }
+    }
+}
