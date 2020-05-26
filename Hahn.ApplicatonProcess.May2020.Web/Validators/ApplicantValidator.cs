03a7d1a68c291fb58252ef1655aa1377073788e6
John C. Kennedy <devjckennedy@gmail.com>
2020-05-26 12:51:55 +0900

    ApplicantValidator

---
+++ Hahn.ApplicatonProcess.May2020.Web/Validators/ApplicantValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Hahn.ApplicatonProcess.May2020.Domain.Models;
+using Hahn.ApplicatonProcess.May2020.Web.Validators.Custom;
+
+namespace Hahn.ApplicatonProcess.May2020.Web.Validators
+{
+    public class ApplicantValidator : AbstractValidator<Applicant>
+    {
+        public ApplicantValidator()
+        {
+            RuleFor(a => a.Name).NotEmpty().MinimumLength(5);
+            RuleFor(a => a.FamilyName).NotEmpty().MinimumLength(5);
+            RuleFor(a => a.Address).NotEmpty().MinimumLength(10);
+            RuleFor(a => a.CountryOfOrigin).ValidCountry();
+            RuleFor(a => a.EmailAddress).EmailAddress();
+            RuleFor(a => a.Age).InclusiveBetween(20, 60);
+            RuleFor(a => a.Hired).NotNull();
+        }
+    }
+}
