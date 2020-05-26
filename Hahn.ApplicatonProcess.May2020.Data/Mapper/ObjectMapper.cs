62be8aeb8a7b2e5b7f45f657853de4af66110b70
John C. Kennedy <devjckennedy@gmail.com>
2020-05-26 19:40:49 +0900

    ObjectMapper

---
+++ Hahn.ApplicatonProcess.May2020.Data/Mapper/ObjectMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AutoMapper;
+using Hahn.ApplicatonProcess.May2020.Data.Models;
+using Hahn.ApplicatonProcess.May2020.Domain.Entities;
+
+namespace Hahn.ApplicatonProcess.May2020.Data.Mapper
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                // This line ensures that internal properties are also mapped over.
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<DTOMapper>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+        public static IMapper Mapper => Lazy.Value;
+    }
+
+    public class DTOMapper : Profile
+    {
+        public DTOMapper()
+        {
+            CreateMap<Applicant, ApplicantModel>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name)).ReverseMap();
+        }
+    }
+}
