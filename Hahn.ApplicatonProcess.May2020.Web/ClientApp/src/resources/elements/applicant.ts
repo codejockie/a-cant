620e5dbf586df4941b81ec5c3289978282edc445
John C. Kennedy <devjckennedy@gmail.com>
2020-05-28 14:39:45 +0900

    handle form submission

--- Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/resources/elements/applicant.ts
+++ Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/resources/elements/applicant.ts
@@ -1,13 +1,28 @@
 import { I18N } from "aurelia-i18n"
+import { Router } from "aurelia-router"
 import { DialogService } from "aurelia-dialog"
 import { inject, NewInstance } from "aurelia-dependency-injection"
-import { ControllerValidateResult, ValidationController, ValidationRules } from "aurelia-validation"
+import {
+  ControllerValidateResult,
+  ValidationController,
+  ValidationRules,
+} from "aurelia-validation"
+import { Http } from "services/http"
+import { Error } from "components/error"
 import { Prompt } from "components/modal"
+import { Applicant as ApplicantModel } from "resources/models/Applicant"
 import { BootstrapFormRenderer } from "resources/validation/bootstrapFormRenderer"
 
-@inject(I18N, DialogService, NewInstance.of(ValidationController))
+@inject(
+  I18N,
+  DialogService,
+  NewInstance.of(ValidationController),
+  NewInstance.of(Http),
+  Router
+)
 export class Applicant {
   heading = "Applicant Form"
+  routeConfig
   applicant = {
     address: "",
     age: "",
@@ -22,22 +37,39 @@ export class Applicant {
     private i18n: any,
     private dialogService,
     private controller: ValidationController,
+    private httpClient: any,
+    private router: Router
   ) {
     this.i18n = i18n
     this.dialogService = dialogService
     this.controller.addRenderer(new BootstrapFormRenderer())
+    this.httpClient = httpClient
+    this.router = router
   }
 
   public bind() {
     const t = (key) => this.i18n.tr(key)
 
-    ValidationRules
-      .ensure("address").required().withMessage(t("errors.address"))
-      .ensure("age").between(19, 61).withMessage(t("errors.invalidAge"))
-      .ensure("countryOfOrigin").required().withMessage(t("errors.countryOfOrigin"))
-      .ensure("emailAddress").required().withMessage(t("errors.email")).email().withMessage(t("errors.invalidEmail"))
-      .ensure("familyName").required().withMessage(t("errors.familyName"))
-      .ensure("name").required().withMessage(t("errors.name"))
+    ValidationRules.ensure("address")
+      .required()
+      .withMessage(t("errors.address"))
+      .ensure("age")
+      .between(19, 61)
+      .withMessage(t("errors.invalidAge"))
+      .ensure("countryOfOrigin")
+      .required()
+      .withMessage(t("errors.countryOfOrigin"))
+      .ensure("emailAddress")
+      .required()
+      .withMessage(t("errors.email"))
+      .email()
+      .withMessage(t("errors.invalidEmail"))
+      .ensure("familyName")
+      .required()
+      .withMessage(t("errors.familyName"))
+      .ensure("name")
+      .required()
+      .withMessage(t("errors.name"))
       .on(this.applicant)
   }
 
@@ -75,29 +107,55 @@ export class Applicant {
   }
 
   reset() {
-    this.dialogService
-      .open({ viewModel: Prompt })
-      .whenClosed((response) => {
-        if (!response.wasCancelled) {
-          this.applicant.address = ""
-          this.applicant.age = ""
-          this.applicant.countryOfOrigin = ""
-          this.applicant.emailAddress = ""
-          this.applicant.familyName = ""
-          this.applicant.hired = ""
-          this.applicant.name = ""
-          this.controller.reset()
-        }
-      })
+    this.dialogService.open({ viewModel: Prompt }).whenClosed((response) => {
+      if (!response.wasCancelled) {
+        this.applicant.address = ""
+        this.applicant.age = ""
+        this.applicant.countryOfOrigin = ""
+        this.applicant.emailAddress = ""
+        this.applicant.familyName = ""
+        this.applicant.hired = ""
+        this.applicant.name = ""
+        this.controller.reset()
+      }
+    })
   }
 
   send() {
+    const {
+      address,
+      age,
+      countryOfOrigin,
+      emailAddress,
+      familyName,
+      hired,
+      name,
+    } = this.applicant
+
+    const applicant: ApplicantModel = {
+      address,
+      age: +age,
+      countryOfOrigin,
+      emailAddress,
+      familyName,
+      hired: Boolean(hired),
+      name,
+    }
     this.controller.validate().then((result: ControllerValidateResult) => {
       if (result.valid) {
-        console.log("Validation successful!")
-      } else {
-        console.log("Validation failed!")
+        this.httpClient.createApplicant(applicant).then((response) => {
+          if (response.status >= 400) {
+            return this.handleError(response.errors)
+          }
+          this.router.navigateToRoute("success", { id: response.id })
+        })
       }
     })
+  }
+
+  handleError(errors: any) {
+    if (errors) {
+      this.dialogService.open({ viewModel: Error, model: errors })
+    }
   }
 }
